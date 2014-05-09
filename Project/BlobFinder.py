f74eddd8718d6e967d0ae19c77994f9676d0515e
Di <dlhoang@calpoly.edu>
2014-05-09 12:36:17 -0700

    Project

--- Project/BlobFinder.py
+++ Project/BlobFinder.py
@@ -1,4 +1,5 @@
-%pylab inline
+
+# %pylab inline
 import matplotlib.pyplot as plt
 from PIL import Image
 import requests
@@ -14,12 +15,12 @@ class Blob():
         self.blob = []
     def add(self,i,j):
         self.blob.append([i,j])
-    def mass():
+    def mass(self):
         return len(self.blob)
-    def distanceTo(blob):
+    def distanceTo(self,blob):
         distance = round(sqrt(((self.centerOfMass()[0] - blob.centerOfMass()[0])**2)\
                         + ((self.centerOfMass()[1] - blob.centerOfMass()[1])**2)),4)
-    def centerOfMass():
+    def centerOfMass(self):
         xtot, ytot = 0, 0
         for pix in self.blob:
             xtot = xtot + pix[0]
@@ -27,31 +28,41 @@ class Blob():
         xcm = float(xtot)/float(len(self.blob))
         ycm = float(ytot)/float(len(self.blob))
         return xcm, ycm
-    
+
+# blob = Blob()
+
+def monochrome(picture,tau):
+    xsize, ysize = picture.size
+    temp = picture.load()
+    for x in range(xsize):
+        for y in range(ysize):
+            r,g,b = temp[x,y]
+            if (r+g+b)/3 >= tau:
+                temp[x,y] = RED
+            else:
+                temp[x,y] = BLACK
+
 def BlobFinder(picture, tau):
     '''find all blobs in the picture using the luminance threshold tau'''
+    monochrome(picture,tau)
     xsize, ysize = picture.size
     temp = picture.load()
     blobs = []
     for x in range(xsize):
         for y in range(ysize):
-            r,g,b = temp[x,y]
-            if r+g+b >= tau:
-                temp[x,y] = RED
-                blob = fill(temp,xsize,ysize,x,y)
+            if temp[x,y] == RED:
+                blob = Fill(temp,xsize,ysize,x,y)
                 blobs.append(blob)
-            else:
-                temp[x,y] = BLACK
     return blobs
-                    
-def fill(picture, xsize, ysize, xstart, ystart):
-    """keep a list of pixels that need to be looked at, 
+    
+def Fill(picture, xsize, ysize, xstart, ystart):
+    """ keep a list of pixels that need to be looked at, 
     but haven't yet been filled in - a list of the (x,y) 
     coordinates of pixels that are neighbors of ones we have 
     already examined.  Keep looping until there's nothing 
     left in this list"""
-    queue = [(xstart,ystart)]
     blob = Blob()
+    queue = [(xstart,ystart)]
     blob.add(xstart,ystart)
     while queue:
         x,y,queue = queue[0][0], queue[0][1], queue[1:]
@@ -71,37 +82,34 @@ def fill(picture, xsize, ysize, xstart, ystart):
                     
 def countBeads(P,picture,tau):
     '''return the number of beads with >= P pixels'''
-    blob = Blob()
     beads = BlobFinder(picture,tau)
     count = 0
-    for blob in beads:
-        if blob.mass >= P:
+    for bead in beads:
+        if bead.mass() >= P:
             count = count + 1
-    return count
+    print count
     
 def getBeads(P,picture,tau):
     '''return all beads with >= P pixels'''
-    blob = Blob()
+    # blob = Blob()
     beads = BlobFinder(picture,tau)
     blobs = []
-    for blob in beads:
-        if blob.mass >=P:
+    for bead in beads:
+        if bead.mass() >=P:
             blobs.append(blob)
     return blobs
              
 def printBeads(P,picture,tau):
-    pic = Image.open(picture)
-    beads = BlobFinder(pic,tau)
+    beads = BlobFinder(picture,tau)
+    for bead in beads:
+        if bead.mass >= P:
+            print "Beads with at least %s pixels" %P
+            print "mass: %s" %bead.mass()
+            print "center of mass: %s %s" %bead.centerOfMass()
     for bead in getBeads(P,picture,tau):
-        if bead.mass() >= P:
-            print "Beads with at least %s pixels" %P + "\n"
-            print "mass: " + bead.mass() + "\n"
-            print "center of mass: " + bead.centerOfMass() + "\n"
-    
-    for bead in getBeads(P,picture,tau):
-        print "All founded beads: " + "\n"
-        print "mass: " + bead.mass() + "\n"
-        print "center of mass: " + bead.centerOfMass() + "\n"
+        print "All founded beads: "
+        print "mass: %s" %bead.mass()
+        print "center of mass: %s %s" %bead.centerOfMass()
         
         
 global ext
@@ -109,6 +117,6 @@ ext = ".jpg"
 
 imageFile = "frame00000.jpg"
 im1 = Image.open(imageFile)
-BlobFinder(im1,250)
+BlobFinder(im1,180)
+countBeads(25,im1,180)
 plt.imshow(im1)
-countBeads(25,im1,250)
