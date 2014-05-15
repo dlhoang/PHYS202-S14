0666384c955edbc0e2a0aec05e6d728eb2217372
Di Hoang <dlhoang@calpoly.edu>
2014-05-15 13:50:39 -0700

    exercises

--- Project/BlobFinder.py
+++ Project/BlobFinder.py
@@ -1,5 +1,4 @@
 
-# %pylab inline
 import matplotlib.pyplot as plt
 from PIL import Image
 import requests
@@ -20,8 +19,8 @@ class Blob():
     
     # ROUND TO 4 DECIMALS
     def distanceTo(self,blob):
-        distance = format(sqrt(((self.centerOfMass()[0] - blob.centerOfMass()[0])**2)\
-                        + ((self.centerOfMass()[1] - blob.centerOfMass()[1])**2)),'.4f')
+        distance = format((((self.centerOfMass()[0] - blob.centerOfMass()[0])**2)\
+                        + ((self.centerOfMass()[1] - blob.centerOfMass()[1])**2))**5,'.4f')
     def centerOfMass(self):
         xtot, ytot = 0, 0
         for pix in self.blob:
@@ -95,7 +94,7 @@ def getBeads(P,picture,tau):
     blobs = []
     for bead in beads:
         if bead.mass() >=P:
-            blobs.append(beads)
+            blobs.append(bead)
     return blobs
              
 def printBeads(P,picture,tau):
