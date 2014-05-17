38dc125e3405820bc6bca83525dc2ad5c04ed1f5
dlhoang <dlhoang@calpoly.edu>
2014-05-17 16:24:57 -0700

    Update BlobFinder.py

--- Project/BlobFinder.py
+++ Project/BlobFinder.py
@@ -19,15 +19,13 @@ class Blob():
     
     # ROUND TO 4 DECIMALS
     def distanceTo(self,blob):
-        <<<<<<< HEAD
         distance = round((((self.centerOfMass()[0] - blob.centerOfMass()[0])**2)\
                         + ((self.centerOfMass()[1] - blob.centerOfMass()[1])**2))**0.5,4)
         return distance
     
-=======
         distance = format((((self.centerOfMass()[0] - blob.centerOfMass()[0])**2)\
                         + ((self.centerOfMass()[1] - blob.centerOfMass()[1])**2))**5,'.4f')
->>>>>>> 0666384c955edbc0e2a0aec05e6d728eb2217372
+
     def centerOfMass(self):
         xtot, ytot = 0, 0
         for pix in self.blob:
